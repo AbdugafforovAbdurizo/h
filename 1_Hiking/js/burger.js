65ee8bfe1742066854be7911493b83760e643ac3
Abdurizo <103021939+AbdugafforovAbdurizo@users.noreply.github.com>
2022-05-04 23:20:18 +0500

    Delete 1_Hiking directory

--- 1_Hiking/js/burger.js
+++
@@ -1,7 +0,0 @@
-$(function  () {
-    $('.burger').click(function (event) {
-        $('.nav').toggleClass('active');
-        $('body').toggleClass('lock');
-        // $('.mobil').css({ 'background':'#f6f6f6fd'}) 
-    })
-})
